b294af1c60caf3468a197642b9f3a8991dd38373
cathy <cathy.siwatala@gmail.com>
2021-10-26 11:05:54 +0200

    Implementation Visitor

--- app/src/main/java/td3/e2/visitors/Visitor.java
+++ app/src/main/java/td3/e2/visitors/Visitor.java
@@ -1,4 +1,14 @@
 package td3.e2.visitors;
 
+import td3.e2.commandes.Client;
+import td3.e2.commandes.Commande;
+import td3.e2.commandes.GroupeClient;
+import td3.e2.commandes.Ligne;
+
 public interface Visitor {
+
+    public void visit(GroupeClient groupeClient);
+    public void visit(Client client);
+    public void visit(Commande commande);
+    public void visit(Ligne ligne);
 }
