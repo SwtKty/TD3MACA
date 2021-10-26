e156c175322fca2c8155459327a50d352f2393fb
cathy <cathy.siwatala@gmail.com>
2021-10-26 11:16:50 +0200

    Implementation PrePostVisitable

--- app/src/main/java/td3/e2/visitable/PrePostVisitable.java
+++ app/src/main/java/td3/e2/visitable/PrePostVisitable.java
@@ -1,4 +1,8 @@
 package td3.e2.visitable;
 
+import td3.e2.visitors.PrePostVisitor;
+import td3.e2.visitors.Visitor;
+
 public interface PrePostVisitable {
+    public void accept(PrePostVisitor prePostVisitor);
 }
