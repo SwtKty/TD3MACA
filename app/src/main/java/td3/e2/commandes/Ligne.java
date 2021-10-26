6c46fca0cf38723781d4a823a2b80e8248a4fa58
cathy <cathy.siwatala@gmail.com>
2021-10-26 11:18:22 +0200

    Implementation Ligne

--- app/src/main/java/td3/e2/commandes/Ligne.java
+++ app/src/main/java/td3/e2/commandes/Ligne.java
@@ -1,4 +1,32 @@
 package td3.e2.commandes;
 
-public class Ligne {
+import td3.e2.visitable.PrePostVisitable;
+import td3.e2.visitable.Visitable;
+import td3.e2.visitors.PrePostVisitor;
+import td3.e2.visitors.Visitor;
+
+public class Ligne implements PrePostVisitable, Visitable {
+
+    private String string;
+    private int entier;
+
+    public Ligne(String string) {
+        this.string = string;
+    }
+
+    public Ligne(String string, int entier) {
+        this.string = string;
+        this.entier = entier;
+    }
+
+
+    @Override
+    public void accept(PrePostVisitor prePostVisitor) {
+
+    }
+
+    @Override
+    public void accept(Visitor visitor) {
+
+    }
 }
